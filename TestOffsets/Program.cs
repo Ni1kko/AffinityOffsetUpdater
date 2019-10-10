08a01ecc8f49317eb2c05dd4ed565136eb053830
Nikko Renolds <ni1kko@outlook.com>
2019-10-10 16:33:51 +0100

    Update Program.cs

--- TestOffsets/Program.cs
+++ TestOffsets/Program.cs
@@ -7,7 +7,19 @@ namespace TestOffsets
     {
        private static void Main()
        {
+           var offsets = new Offsets();
 
-       }
+            #region NETVARS
+            Console.WriteLine("[NETVARS]" + Environment.NewLine);
+            //Console.WriteLine(nameof(offsets.rootObject.netvars.) + ": " + offsets.rootObject.netvars.);
+            Console.WriteLine(Environment.NewLine);
+            #endregion
+
+            #region SIGNATURES
+            Console.WriteLine("[SIGNATURES]" + Environment.NewLine);
+            //Console.WriteLine(nameof(offsets.rootObject.signatures.) + ": " + offsets.rootObject.signatures.);
+            Console.WriteLine(Environment.NewLine);
+            #endregion
+        }
     }
 }
